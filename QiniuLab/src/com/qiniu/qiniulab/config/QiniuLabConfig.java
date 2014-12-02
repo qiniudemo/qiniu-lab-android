90edf76b91d17a32063a7b8fa84f6dfc785ab5bb
Jemy <jemygraw@gmail.com>
2014-12-02 23:06:05 +0800

    Add file chooser module. Add simple upload without key example.

--- QiniuLab/src/com/qiniu/qiniulab/config/QiniuLabConfig.java
+++ QiniuLab/src/com/qiniu/qiniulab/config/QiniuLabConfig.java
@@ -1,8 +1,15 @@
 package com.qiniu.qiniulab.config;
 
 public class QiniuLabConfig {
+	public final static byte[] EMPTY_BODY = new byte[0];
+	public final static String REMOTE_SERVICE_SERVER = "http://192.168.100.108/~jemy/qiniu-lab-php";
 
-	public static String REMOTE_SERVICE_SERVER = "http://localhost/~jemy/qiniu-lab-php";
-	
-	public final static String SIMPLE_UPLOAD_WITHOUT_KEY_PATH="/demos/api/simple_upload_without_key_token.php";
+	public final static String SIMPLE_UPLOAD_WITHOUT_KEY_PATH = "/demos/api/simple_upload_without_key_upload_token.php";
+
+	public static String makeUrl(String remoteServer, String reqPath) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(remoteServer);
+		sb.append(reqPath);
+		return sb.toString();
+	}
 }
