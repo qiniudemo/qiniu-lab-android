dad5a0bf3833cb7dbb1b074fb5e1267c5c4d220c
Jemy <jemygraw@gmail.com>
2014-12-05 17:26:23 +0800

    Add log to the simple video play .

--- QiniuLab/src/com/qiniu/qiniulab/activity/SimpleVideoPlayActivity.java
+++ QiniuLab/src/com/qiniu/qiniulab/activity/SimpleVideoPlayActivity.java
@@ -6,13 +6,16 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.MediaController;
+import android.widget.TextView;
 import android.widget.VideoView;
 
 import com.qiniu.qiniulab.R;
+import com.qiniu.qiniulab.utils.Tools;
 
 public class SimpleVideoPlayActivity extends ActionBarActivity {
 	private VideoView videoPlayView;
 	private MediaController videoPlayController;
+	private TextView videoPlayLogTextView;
 
 	public SimpleVideoPlayActivity() {
 	}
@@ -28,6 +31,8 @@ public class SimpleVideoPlayActivity extends ActionBarActivity {
 		this.videoPlayController = new MediaController(this);
 		this.videoPlayView = (VideoView) this
 				.findViewById(R.id.simple_video_play_videoview);
+		this.videoPlayLogTextView = (TextView) this
+				.findViewById(R.id.simple_video_play_log_textview);
 		videoPlayView.setMediaController(videoPlayController);
 		videoPlayController.setMediaPlayer(videoPlayView);
 		videoPlayController.setAnchorView(videoPlayView);
@@ -35,12 +40,18 @@ public class SimpleVideoPlayActivity extends ActionBarActivity {
 		String videoUrl = this.getIntent().getStringExtra("VideoUrl");
 		this.setTitle(videoName);
 		videoPlayView.setVideoURI(Uri.parse(videoUrl));
+		final long startTime = System.currentTimeMillis();
 		videoPlayView.setOnPreparedListener(new OnPreparedListener() {
 
 			@Override
 			public void onPrepared(MediaPlayer mp) {
+				long endTime = System.currentTimeMillis();
+				long loadTime = endTime - startTime;
+				videoPlayLogTextView.append("Load Time: "
+						+ Tools.formatMilliSeconds(loadTime) + "\r\n");
 				mp.start();
 			}
 		});
+
 	}
 }
