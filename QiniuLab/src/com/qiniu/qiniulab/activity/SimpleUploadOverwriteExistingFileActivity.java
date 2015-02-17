8d708e444556e1e3b912b956c3b3041e08bcf716
Jemy <jemygraw@gmail.com>
2015-02-17 21:10:33 +0800

    Update simple upload overwrite file example to follow new logic and ui design.

--- QiniuLab/src/com/qiniu/qiniulab/activity/SimpleUploadOverwriteExistingFileActivity.java
+++ QiniuLab/src/com/qiniu/qiniulab/activity/SimpleUploadOverwriteExistingFileActivity.java
@@ -22,6 +22,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.ipaulpro.afilechooser.utils.FileUtils;
+import com.qiniu.android.common.Config;
 import com.qiniu.android.http.CompletionHandler;
 import com.qiniu.android.http.HttpManager;
 import com.qiniu.android.http.ResponseInfo;
@@ -29,6 +30,7 @@ import com.qiniu.android.storage.UpCompletionHandler;
 import com.qiniu.android.storage.UpProgressHandler;
 import com.qiniu.android.storage.UploadManager;
 import com.qiniu.android.storage.UploadOptions;
+import com.qiniu.android.utils.AsyncRun;
 import com.qiniu.qiniulab.R;
 import com.qiniu.qiniulab.config.QiniuLabConfig;
 import com.qiniu.qiniulab.utils.Tools;
@@ -37,8 +39,6 @@ public class SimpleUploadOverwriteExistingFileActivity extends
 		ActionBarActivity {
 	private SimpleUploadOverwriteExistingFileActivity context;
 	private EditText uploadFileKeyEditText;
-	private TextView uploadTokenTextView;
-	private TextView uploadFileTextView;
 	private TextView uploadLogTextView;
 	private LinearLayout uploadStatusLayout;
 	private ProgressBar uploadProgressBar;
@@ -49,8 +49,9 @@ public class SimpleUploadOverwriteExistingFileActivity extends
 	private UploadManager uploadManager;
 	private static final int REQUEST_CODE = 8090;
 	private long uploadLastTimePoint;
-	private long uploadLastPos;
+	private long uploadLastOffset;
 	private long uploadFileLength;
+	private String uploadFilePath;
 
 	public SimpleUploadOverwriteExistingFileActivity() {
 		this.context = this;
@@ -60,10 +61,6 @@ public class SimpleUploadOverwriteExistingFileActivity extends
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.simple_upload_overwrite_existing_file_activity);
-		this.uploadTokenTextView = (TextView) this
-				.findViewById(R.id.simple_upload_overwrite_existing_file_token);
-		this.uploadFileTextView = (TextView) this
-				.findViewById(R.id.simple_upload_overwrite_existing_file_file);
 		this.uploadFileKeyEditText = (EditText) this
 				.findViewById(R.id.simple_upload_overwrite_existing_file_key);
 		this.uploadProgressBar = (ProgressBar) this
@@ -81,56 +78,6 @@ public class SimpleUploadOverwriteExistingFileActivity extends
 		this.uploadLogTextView = (TextView) this
 				.findViewById(R.id.simple_upload_overwrite_existing_file_log_textview);
 
-	}
-
-	public void getUploadToken(View view) {
-		if (this.httpManager == null) {
-			this.httpManager = new HttpManager();
-		}
-		// get upload token with key
-		String key = this.uploadFileKeyEditText.getText().toString().trim();
-		if (key.length() == 0) {
-			Toast.makeText(context,
-					context.getString(R.string.qiniu_specify_the_file_key),
-					Toast.LENGTH_LONG).show();
-			return;
-		}
-		byte[] postData = QiniuLabConfig.EMPTY_BODY;
-		try {
-			postData = new String("key=" + key).getBytes("utf-8");
-		} catch (UnsupportedEncodingException e1) {
-			Log.e("SystemError", "no supported charset utf-8");
-		}
-		Header contentTypeHeader = new BasicHeader("Content-Type",
-				"application/x-www-form-urlencoded");
-		this.httpManager.postData(QiniuLabConfig.makeUrl(
-				QiniuLabConfig.REMOTE_SERVICE_SERVER,
-				QiniuLabConfig.SIMPLE_UPLOAD_OVERWRITE_EXISTING_FILE_PATH),
-				postData, new Header[] { contentTypeHeader }, null,
-				new CompletionHandler() {
-
-					@Override
-					public void complete(ResponseInfo respInfo,
-							JSONObject jsonData) {
-						if (respInfo.statusCode == 200) {
-							try {
-								String uploadToken = jsonData
-										.getString("uptoken");
-								uploadTokenTextView.setText(uploadToken);
-							} catch (JSONException e) {
-								Toast.makeText(
-										context,
-										context.getString(R.string.qiniu_get_upload_token_failed),
-										Toast.LENGTH_LONG).show();
-							}
-						} else {
-							Toast.makeText(
-									context,
-									context.getString(R.string.qiniu_get_upload_token_failed),
-									Toast.LENGTH_LONG).show();
-						}
-					}
-				});
 	}
 
 	public void selectUploadFile(View view) {
@@ -155,7 +102,11 @@ public class SimpleUploadOverwriteExistingFileActivity extends
 					try {
 						// Get the file path from the URI
 						final String path = FileUtils.getPath(this, uri);
-						this.uploadFileTextView.setText(path);
+						this.uploadFilePath = path;
+						this.clearLog();
+						this.writeLog(context
+								.getString(R.string.qiniu_select_upload_file)
+								+ path);
 					} catch (Exception e) {
 						Toast.makeText(
 								context,
@@ -170,48 +121,98 @@ public class SimpleUploadOverwriteExistingFileActivity extends
 	}
 
 	public void uploadFile(View view) {
+		if (this.uploadFilePath == null) {
+			return;
+		}
+		if (this.httpManager == null) {
+			this.httpManager = new HttpManager();
+		}
+		// get upload token with key
+		final String uploadFileKey = this.uploadFileKeyEditText.getText()
+				.toString().trim();
+		if (uploadFileKey.length() == 0) {
+			Toast.makeText(context,
+					context.getString(R.string.qiniu_specify_the_file_key),
+					Toast.LENGTH_LONG).show();
+			return;
+		}
+		byte[] postData = QiniuLabConfig.EMPTY_BODY;
+		try {
+			postData = new String("key=" + uploadFileKey).getBytes("utf-8");
+		} catch (UnsupportedEncodingException e1) {
+			Log.e("SystemError", "no supported charset utf-8");
+		}
+		Header contentTypeHeader = new BasicHeader("Content-Type",
+				"application/x-www-form-urlencoded");
+		this.httpManager.postData(QiniuLabConfig.makeUrl(
+				QiniuLabConfig.REMOTE_SERVICE_SERVER,
+				QiniuLabConfig.SIMPLE_UPLOAD_OVERWRITE_EXISTING_FILE_PATH),
+				postData, new Header[] { contentTypeHeader }, null,
+				new CompletionHandler() {
+
+					@Override
+					public void complete(ResponseInfo respInfo,
+							JSONObject jsonData) {
+						if (respInfo.statusCode == 200) {
+							try {
+								String uploadToken = jsonData
+										.getString("uptoken");
+								writeLog(context
+										.getString(R.string.qiniu_get_upload_token)
+										+ uploadToken);
+								upload(uploadFileKey, uploadToken);
+							} catch (JSONException e) {
+								Toast.makeText(
+										context,
+										context.getString(R.string.qiniu_get_upload_token_failed),
+										Toast.LENGTH_LONG).show();
+								writeLog(context
+										.getString(R.string.qiniu_get_upload_token_failed)
+										+ respInfo.toString());
+								if (jsonData != null) {
+									writeLog(jsonData.toString());
+								}
+							}
+						} else {
+							Toast.makeText(
+									context,
+									context.getString(R.string.qiniu_get_upload_token_failed),
+									Toast.LENGTH_LONG).show();
+							writeLog(context
+									.getString(R.string.qiniu_get_upload_token_failed)
+									+ respInfo.toString());
+							if (jsonData != null) {
+								writeLog(jsonData.toString());
+							}
+
+						}
+					}
+				});
+	}
+
+	private void upload(String uploadFileKey, String uploadToken) {
 		if (this.uploadManager == null) {
 			this.uploadManager = new UploadManager();
 		}
-		String uploadToken = this.uploadTokenTextView.getText().toString();
-		File uploadFile = new File(this.uploadFileTextView.getText().toString());
-		String uploadFileKey = this.uploadFileKeyEditText.getText().toString();
+		File uploadFile = new File(this.uploadFilePath);
 		UploadOptions uploadOptions = new UploadOptions(null, null, false,
 				new UpProgressHandler() {
-
 					@Override
 					public void progress(String key, double percent) {
-						int percentage = (int) (percent * 100);
-						uploadProgressBar.setProgress(percentage);
-						uploadPercentageTextView.setText(percentage + " %");
-						long uploadCurrentPos = (long) (uploadFileLength * percent);
-						long uploadCurrentMillis = System.currentTimeMillis();
-						long uploadSliceSize = uploadCurrentPos - uploadLastPos;
-						long uploadSliceMillis = uploadCurrentMillis
-								- uploadLastTimePoint;
-
-						if (uploadSliceMillis != 0) {
-							uploadSpeedTextView
-									.setText((uploadSliceSize / uploadSliceMillis)
-											+ " KB/s");
-						}
-						// update pos
-						uploadLastTimePoint = uploadCurrentMillis;
-						uploadLastPos = uploadCurrentPos;
+						updateStatus(percent);
 					}
-
 				}, null);
 		final long startTime = System.currentTimeMillis();
 		final long fileLength = uploadFile.length();
 		this.uploadFileLength = fileLength;
 		this.uploadLastTimePoint = startTime;
-		this.uploadLastPos = 0;
+		this.uploadLastOffset = 0;
 		// prepare status
 		uploadPercentageTextView.setText("0 %");
 		uploadSpeedTextView.setText("0 KB/s");
 		uploadFileLengthTextView.setText(Tools.formatSize(fileLength));
 		uploadStatusLayout.setVisibility(LinearLayout.VISIBLE);
-
+		writeLog(context.getString(R.string.qiniu_upload_file) + "...");
 		this.uploadManager.put(uploadFile, uploadFileKey, uploadToken,
 				new UpCompletionHandler() {
 					@Override
@@ -227,56 +228,78 @@ public class SimpleUploadOverwriteExistingFileActivity extends
 							try {
 								String fileKey = jsonData.getString("key");
 								String fileHash = jsonData.getString("hash");
-								uploadLogTextView.append("File Size: "
-										+ Tools.formatSize(uploadFileLength)
-										+ "\r\n");
-								uploadLogTextView.append("File Key: " + fileKey
-										+ "\r\n");
-
-								uploadLogTextView.append("File Hash: "
-										+ fileHash + "\r\n");
-								uploadLogTextView.append("Last Time: "
-										+ Tools.formatMilliSeconds(lastMillis)
-										+ "\r\n");
-								uploadLogTextView.append("Average Speed: "
-										+ (fileLength / lastMillis)
-										+ " KB/s\r\n");
-								uploadLogTextView.append("StatusCode: "
-										+ respInfo.statusCode + "\r\n");
-								uploadLogTextView.append("Reqid: "
-										+ respInfo.reqId + "\r\n");
-								uploadLogTextView
-										.append("---------------------------\r\n");
-
+								writeLog("File Size: "
+										+ Tools.formatSize(uploadFileLength));
+								writeLog("File Key: " + fileKey);
+								writeLog("File Hash: " + fileHash);
+								writeLog("Last Time: "
+										+ Tools.formatMilliSeconds(lastMillis));
+								writeLog("Average Speed: "
+										+ Tools.formatSpeed(fileLength,
+												lastMillis));
+								writeLog("X-Reqid: " + respInfo.reqId);
+								writeLog("X-Log: " + respInfo.xlog);
+								writeLog("X-Via: " + respInfo.xvia);
+								writeLog("--------------------------------");
 							} catch (JSONException e) {
 								Toast.makeText(
 										context,
 										context.getString(R.string.qiniu_upload_file_response_parse_error),
 										Toast.LENGTH_LONG).show();
-								uploadLogTextView.append(jsonData.toString());
-								uploadLogTextView.append("\r\n");
-								uploadLogTextView
-										.append("---------------------------\r\n");
+								writeLog(context
+										.getString(R.string.qiniu_upload_file_response_parse_error));
+								if (jsonData != null) {
+									writeLog(jsonData.toString());
+								}
+								writeLog("--------------------------------");
 							}
 						} else {
 							Toast.makeText(
 									context,
 									context.getString(R.string.qiniu_upload_file_failed),
 									Toast.LENGTH_LONG).show();
-
-							uploadLogTextView.append("StatusCode: "
-									+ respInfo.statusCode + "\r\n");
-							uploadLogTextView.append("Reqid: " + respInfo.reqId
-									+ "\r\n");
-							uploadLogTextView.append("Xlog: " + respInfo.xlog
-									+ "\r\n");
-							uploadLogTextView.append("Error: " + respInfo.error
-									+ "\r\n");
-							uploadLogTextView
-									.append("---------------------------\r\n");
+							writeLog(respInfo.toString());
+							if (jsonData != null) {
+								writeLog(jsonData.toString());
+							}
+							writeLog("--------------------------------");
 						}
 					}
 
 				}, uploadOptions);
+	}
+
+	private synchronized void updateStatus(final double percentage) {
+		long now = System.currentTimeMillis();
+		long deltaTime = now - uploadLastTimePoint;
+		long currentOffset = (long) (percentage * uploadFileLength);
+		long deltaSize = currentOffset - uploadLastOffset;
+		if (deltaTime <= 0 || deltaSize < Config.CHUNK_SIZE) {
+			return;
+		}
+
+		final String speed = Tools.formatSpeed(deltaSize, deltaTime);
+		// update
+		uploadLastTimePoint = now;
+		uploadLastOffset = currentOffset;
+
+		AsyncRun.run(new Runnable() {
+			@Override
+			public void run() {
+				int progress = (int) (percentage * 100);
+				uploadProgressBar.setProgress(progress);
+				uploadPercentageTextView.setText(progress + " %");
+				uploadSpeedTextView.setText(speed);
+			}
+		});
+	}
+
+	private void clearLog() {
+		this.uploadLogTextView.setText("");
+	}
+
+	private void writeLog(String msg) {
+		this.uploadLogTextView.append(msg);
+		this.uploadLogTextView.append("\r\n");
 	}
 }
